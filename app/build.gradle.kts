4645d6cf817dd702c86092bb4c74c84ef1039762
octmicy <2335260621@qq.com>
2026-09-05 13:10:48 +0800

    v0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "cn.edu.xyc.campus"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     signingConfigs {
